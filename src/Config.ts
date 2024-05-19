578854a52ad5a5e5e8051f4fba8df8fe34efc417
Vander Moleker <noierdev@proton.me>
2024-05-19 22:11:20 +0200

    backend

--- src/Config.ts
+++ src/Config.ts
@@ -1,3 +1,3 @@
-export const BACKEND_URL="http://localhost:8001/api"
-// export const BACKEND_URL="https://cods.land/solgold/api"
+// export const BACKEND_URL="http://localhost:8222/api"
+export const BACKEND_URL="https://cods.land/solgold/api"
 export const MINT="HbNSiv68ZhjBRQ415BsvL2GYrye7NJ7CzDDXBvC2VjyD"
